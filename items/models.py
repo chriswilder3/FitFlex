9bbb07f039fb8c5755dea159a77a6d319e254e17
chriswilder3 <chriswilderdelta19@gmail.com>
2024-11-17 03:34:38 +0530

    added new model Item, modified view items

--- items/models.py
+++ items/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+            # this module provides classes and methods used
+            # to define database models.
 
-# Create your models here.
+
+# IMP : Django follows ORM
+
+class Item(models.Model):
+            # Defines a class Item that inherits from another class 
+            # called Model from models module. 
+            # By doing this Item became a Django model, based on which
+            # django will create a corresponding table of same
+            # name, item (lowercased) in the DB.
+        
+        # As beginner, We will have a name, price for each item. But note
+        # that Django will automatically create primary key field 'id'.
+
+        name = models.CharField( max_length = 255)
+            # Here name is a model field which represents a column in DB
+            # models.CharField() is a field for storing char based data.
+            # Note that max_length is required for CharField()
+
+        price = models.IntegerField() # No need of arguments for integer
+
+# IMP Note :
+
+# The name of the table will be derived from the class name,
+# converted to lowercase (e.g., item for the Item model). override this
+# behavior by specifying the db_table option inside the model’s Meta class
+
+
+# To apply the model and create the actual table in the database, you 
+# need to run migrations. When you add a new model or modify existing 
+# models, Django generates migration files to reflect these changes in
+# the database schema.
+
+# First, run: python manage.py makemigrations to generate migration
+#  files.
+# Then, apply the migrations with: python manage.py migrate
+
+# The model can be queried and manipulated using Django’s ORM 
+# (Object-Relational Mapping) in the views
+
--- items/views.py
+++ items/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render # used for rendering HTML template
                     # and returning Http Response object, but not in this 
                     # program
 from django.http import HttpResponse
-                    # used to send small text http responses
+                    # used to send http responses(HTML, JSON, text) back to
+                    # the browser/ client
+                
+from django.template import loader
+                # django.template package and its loader module provide
+                # Django's template system. They facilitate the use of 
+                # templates for dynamically rendering HTML or other content.
 
 # Create your views here.
 def sample( request ):     # request object is instance of Http request and
@@ -14,4 +20,26 @@ def sample( request ):     # request object is instance of Http request and
 
 
 def items(request):
-    
+    itemTemplate = loader.get_template('items.html')
+                # Retrieves a template file named items.html located in 
+                # the TEMPLATES directories as configured in the settings.py 
+                # Django searches for the file in the that directory.
+
+    return HttpResponse( itemTemplate.render())
+                # The render method of the template object processes the loaded
+                # HTML template and returns a string containing HTMl content
+
+                # This string containg HTML content is wrapped as HttpResponse
+                # and passed to browser which will unpack into HTML
+
+                # In further views we will see render() completely
+         # render() method takes two arguments:
+         
+                # context (optional): A dictionary that contains the 
+                # dynamic data to replace placeholders (variables) in the
+                # template.
+
+                # request (optional but recommended): The HTTP request 
+                # object, which provides additional context to the 
+                # template (for things like URL reversing and accessing 
+                # user information).

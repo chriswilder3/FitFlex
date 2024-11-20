4a92212ca0858c957f26a052659642c4c6f67c1e
chriswilder3 <chriswilderdelta19@gmail.com>
2024-11-20 20:06:45 +0530

    style the nav bar

--- items/models.py
+++ items/models.py
@@ -21,7 +21,17 @@ class Item(models.Model):
             # models.CharField() is a field for storing char based data.
             # Note that max_length is required for CharField()
 
-        price = models.IntegerField() # No need of arguments for integer
+        price = models.DecimalField(max_digits=10, decimal_places=2) 
+
+        description = models.TextField()
+        
+        category = models.CharField( max_length = 255)
+
+        sub_category = models.CharField( max_length = 255, blank= True)
+
+        item_type = models.CharField( max_length = 255)
+
+        image = models.ImageField( blank= True)
 
         def __str__(self):
             # When you display a Model as a list, Django displays each 
